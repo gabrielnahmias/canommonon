9ddf7aa04ae08a20db75b8243755060760754d67
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2012-07-02 14:27:21 -0500

    Employs Selectify (http://github.com/terrasoftlabs/jquery-selectify).

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$( function() {
+	
+	$("code").selectify( {
+		
+		title: "Click to select code"
+		
+	} );
+	
+	$("#results").selectify( {
+		
+		title: "Click to select results"
+		
+	} );
+	
+} );
