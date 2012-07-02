ccaa9fafe6527ab97986fc09cb6067f812a59e9d
Gabriel Nahmias <gabriel@terrasoftlabs.com>
2012-07-02 14:21:29 -0500

    Started defining constants for repetitive values, added a new form of array_intersect (array_intersect_exact) which accounts for duplicates, and heavily modified the displayArray() function (added JavaScript and Ruby array forms).

--- inc/functions.php
+++ inc/functions.php
@@ -1,5 +1,29 @@
 <?php
 
+define("TEXT_SEPARATOR", ", ");
+
+function array_intersect_exact($array1, $array2) {
+	
+	// This compensates for duplicates.
+	
+	$result = array();
+	
+	foreach ($array1 as $val) {
+		
+		if ( ( $key = array_search($val, $array2, TRUE) ) !== false ) {
+			
+			$result[] = $val;
+			
+			unset( $array2[$key] );
+			
+		}
+		
+	}
+	
+	return $result;
+	
+}
+
 function displayArray($a, $bAsCode, $sLanguage, $aRange) {
 	
 	// Display array as either a plain list or code for an array.
@@ -17,24 +41,74 @@ function displayArray($a, $bAsCode, $sLanguage, $aRange) {
 	if ( isset($aRange) )
 		$a = array_slice($a, $aRange[0], $aRange[1]);
 	
-	if ($bAsCode): ?><code><span class="variable"><?php if ($sLanguage == "php"): ?>$<?php endif; ?>aCodes</span> <span class="operator">=</span> <span class="keyword"><?php if ($sLanguage == "php"): ?>a<?php elseif($sLanguage == "vb"): ?>A<?php endif; ?>rray</span><span class="bracket">(</span><?php endif;
+	if ($bAsCode): ?>
+		
+		<code>
+			
+			<?php if ($sLanguage == "js"): ?>
+				
+				<span class="keyword">var</span>
+				
+			<? endif; ?>
+			
+			<span class="variable">
+				
+				<?php if ($sLanguage == "php"): ?>$<?php endif; ?>aCodes
+				
+			</span>
+			
+			<span class="operator">=</span>
+			
+			<?php if ($sLanguage != "js" && $sLanguage != "rb"): ?>
+				
+				<span class="keyword">
+					
+					<?php if ($sLanguage == "php"): ?>
+						
+						a<?php elseif($sLanguage == "vb"): ?>A<?php endif; ?>rray</span><? endif; ?><span class="bracket"><?php if ($sLanguage != "vb" && $sLanguage != "php"): ?>[<?php else: ?>(<?php endif;?></span><?php
+			
+			// Output contents of array.
+			
+			foreach($a as $sValue) {
+				
+				$bMultiple = false;
+				
+				$iOccurrences = count( array_keys($a, $sValue) );
+				
+				if ( $iOccurrences > 1 )
+					$bMultiple = true;
+				
+				// NEED TO DO SOMETHING WITH $bMultiple!!!!!!!  Add some notation or whatever.  Prevent it from being
+				// output again?
+				
+				?><span class="string">"<?php
+				
+				print $sValue;
+				
+				?>"</span><?php
+				
+				$iCount++;
+				
+				// If it's not the last value in the array, add a separator after the output.
+				
+				if ( $iCount != count($a) )
+					print TEXT_SEPARATOR;
+				
+			}
+			
+			?><span class="bracket"><?php if ($sLanguage != "vb" && $sLanguage != "php"): ?>]<?php else: ?>)<?php endif;?></span><?php if ($sLanguage == "php" || $sLanguage == "js"): ?>;<?php endif; ?>
+			
+		</code>
+		
+	<?php
 	
-	foreach($a as $sValue) {
+	else:
 		
-		if ($bAsCode): ?><span class="string">"<?php endif;
+		// If it's not code, simply implode it with the proper separator.
 		
-		print $sValue;
+		print implode(TEXT_SEPARATOR, $a);
 		
-		if ($bAsCode): ?>"</span><?php endif;
-		
-		$iCount++;
-		
-		if ( array_key_exists($iCount + 1, $a) )
-			print ", ";
-		
-	}
-	
-	if ($bAsCode): ?><span class="bracket">)</span><?php if ($sLanguage == "php"): ?>;<?php endif; ?></code><?php endif;
+	endif;
 	
 }
 
@@ -43,5 +117,20 @@ function lsort($a, $b) {
 	// Function to pass to usort that sorts by length.
 	
 	return strlen($b) - strlen($a);
+	
+}
+
+function mark($sType) {
+	
+	import_request_variables("g");
+	
+	$sWord = "";
+	
+	if ($sType == "select" || $sType == "radio")
+		$sWord = "selected";
+	elseif ($sType == "checkbox")
+		$sWord = "checked";
+	
+	print " $sWord=\"$sWord\"";
 	
 }
